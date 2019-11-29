8f3d112558cda1e56d7693cf3db027f3531b07f2
D065023 <david.kunz@sap.com>
2019-11-29 12:11:29 +0100

    prettier

--- packages/bookshop/srv/cat-service.js
+++ packages/bookshop/srv/cat-service.js
@@ -2,25 +2,42 @@ const cds = require('@sap/cds')
 const { Books } = cds.entities
 
 /** Service implementation for CatalogService */
-module.exports = cds.service.impl(function() {
-  this.after ('READ', 'Books', each => each.stock > 111 && _addDiscount2(each,11))
-  this.before ('CREATE', 'Orders', _reduceStock)
+module.exports = cds.service.impl(function () {
+  this.after(
+    'READ',
+    'Books',
+    each => each.stock > 111 && _addDiscount2(each, 11)
+  )
+  this.before('CREATE', 'Orders', _reduceStock)
 })
 
 /** Add some discount for overstocked books */
-function _addDiscount2 (each,discount) {
+function _addDiscount2 (each, discount) {
   each.title += ` -- ${discount}% discount!`
 }
 
 /** Reduce stock of ordered books if available stock suffices */
 async function _reduceStock (req) {
   const { Items: OrderItems } = req.data
-  return cds.transaction(req) .run (()=> OrderItems.map (order =>
-    UPDATE (Books) .set ('stock -=', order.amount)
-    .where ('ID =', order.book_ID) .and ('stock >=', order.amount)
-  )) .then (all => all.forEach ((affectedRows,i) => {
-    if (affectedRows === 0)  req.error (409,
-      `${OrderItems[i].amount} exceeds stock for book #${OrderItems[i].book_ID}`
+  return cds
+    .transaction(req)
+    .run(() =>
+      OrderItems.map(order =>
+        UPDATE(Books)
+          .set('stock -=', order.amount)
+          .where('ID =', order.book_ID)
+          .and('stock >=', order.amount)
+      )
     )
-  }))
+    .then(all =>
+      all.forEach((affectedRows, i) => {
+        if (affectedRows === 0)
+          req.error(
+            409,
+            `${OrderItems[i].amount} exceeds stock for book #${
+              OrderItems[i].book_ID
+            }`
+          )
+      })
+    )
 }
