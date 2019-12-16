d678b51320ef9236f139e76ef49fdd3f4b1b5795
D065023 <david.kunz@sap.com>
2019-12-16 21:52:43 +0100

    revert prefix

--- packages/bookshop/srv/admin-service.js
+++ packages/bookshop/srv/admin-service.js
@@ -4,7 +4,7 @@ const { Books, ShippingAddresses } = cds.entities
 
 const bupaSrv = cds.connect.to('API_BUSINESS_PARTNER')
 
-bupaSrv.on('Changed', 'BusinessPartner', async msg => {
+bupaSrv.on('sap/S4HANAOD/c532/BO/BusinessPartner/Changed', async msg => {
   console.log('>> Message:', msg.data)
 
   const BusinessPartner = msg.data.KEY[0].BUSINESSPARTNER
