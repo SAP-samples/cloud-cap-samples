74c155ca62adb811bbdc8ceb4981256f92ac86c1
D065023 <david.kunz@sap.com>
2019-12-16 21:56:32 +0100

    better

--- packages/bookshop/srv/external/API_BUSINESS_PARTNER.js
+++ packages/bookshop/srv/external/API_BUSINESS_PARTNER.js
@@ -1,13 +1,11 @@
 module.exports = srv => {
-
-    srv.on('UPDATE', req => {
-
-        const payload = {
-            KEY: [{ BUSINESSPARTNER: req.data.BusinessPartner }]
-        }
-        console.log('<< Message:', payload)
-        srv.emit('sap/S4HANAOD/c532/BO/BusinessPartner/Changed', payload)
-
+  srv.on('UPDATE', req => {
+    const payload = {
+      KEY: [{ BUSINESSPARTNER: req.data.BusinessPartner }]
+    }
+    console.log('<< Message:', payload)
+    req.on('succeeded', () => {
+      srv.emit('sap/S4HANAOD/c532/BO/BusinessPartner/Changed', payload)
     })
-
+  })
 }
