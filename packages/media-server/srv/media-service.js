898b09fe382c7294c5a8908e64d838c799ecb959
Lakshmi C Rajeev <lakshmi.c.rajeev@sap.com>
2019-12-19 10:43:22 +0530

    Update media-service.js

--- packages/media-server/srv/media-service.js
+++ packages/media-server/srv/media-service.js
@@ -1,26 +1,25 @@
 module.exports = (srv) => {
-
     const loki = require('lokijs');
     const db = new loki('DB');
-    const mediaDB= db.addCollection("Media");
+    const mediaDB = db.addCollection("Media");
     const { Readable, PassThrough } = require('stream')
 
     srv.before('CREATE', 'Media', (req) => {
-        var obj = mediaDB.insert({media: ""});
-        req.data.id=obj.$loki;
+        const obj = mediaDB.insert({ media: "" });
+        req.data.id = obj.$loki;
     })
 
-    srv.on ('UPDATE', 'Media', async (req,next) => {
+    srv.on('UPDATE', 'Media', (req, next) => {
         try{ 
-            var url = req._.req.path;
+            const url = req._.req.path;
             if (url.indexOf("content")>0) {
                 const id = req.data.id; 
-                var obj = mediaDB.get(id);
+                const obj = mediaDB.get(id);
                 if(obj==null) {
                     req.reject(404,"No record found for the ID");
                     return;
                 }
-                var stream = new PassThrough();
+                const stream = new PassThrough();
                 let chunks = [];
                 stream.on('data',  (chunk) => {
                     chunks.push(chunk);
@@ -36,47 +35,48 @@ module.exports = (srv) => {
             console.log(error)
             return 
         }
-    }) 
 
-    srv.on("READ","Media", (req,next)=>{
-        try{
-            var url = req._.req.path;
-            if (url.indexOf("content")>0) {
-                const id = req.data.id; 
-                if(mediaDB.get(id)==null) {
-                    req.reject(404,"Media not found for the ID");
-                    return ;
+    })
+
+    srv.on("READ", "Media", (req, next) => {
+        try {
+            const url = req._.req.path;
+            if (url.indexOf("content") > 0) {
+                const id = req.data.id;
+                if (mediaDB.get(id) == null) {
+                    req.reject(404, "Media not found for the ID");
+                    return;
                 }
-                var decodedMedia  = new Buffer( mediaDB.get(id).media.split(';base64,').pop(), 'base64');
+                const decodedMedia = new Buffer(mediaDB.get(id).media.split(';base64,').pop(), 'base64');
                 return _formatResult(decodedMedia);
             }
             else return next()  //> delegate to next/default handlers
-        }catch(error){
-            req.reject(404,"Media DB error")
-            return 
+        } catch (error) {
+            req.reject(404, "Media DB error")
+            return
         }
-      })  
+    })
 
-      srv.on("DELETE","Media", (req,next)=>{
-        try{
-            const id = req.data.id; 
-            var obj=mediaDB.get(id);
-            if(obj!=null) {
+    srv.on("DELETE", "Media", (req, next) => {
+        try {
+            const id = req.data.id;
+            const obj = mediaDB.get(id);
+            if (obj != null) {
                 mediaDB.remove(obj)
             }
             return next()  //> delegate to next/default handlers
-        }catch(error){
-            req.reject(404,"Media DB error ")
+        } catch (error) {
+            req.reject(404, "Media DB error ")
         }
-      })  
+    })
 
-    function _formatResult (decodedMedia)  {
-        var readable= new Readable
-        var result = new Array()
+    function _formatResult(decodedMedia) {
+        const readable = new Readable
+        const result = new Array()
         readable.push(decodedMedia)
-        readable.push(null) 
-        result.push({value:readable})
-        return result 
-      }
+        readable.push(null)
+        result.push({ value: readable })
+        return result
+    }
 
-  }
+}
