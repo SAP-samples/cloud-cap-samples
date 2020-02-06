9932d02d5765280b1b4feadf24b2a9f524087e9d
pianocktail <matthias.buehl@sap.com>
2020-02-06 08:27:57 +0000

    CF enablement

---
+++ packages/bookshop/srv/admin-service.js
@@ -0,0 +1,10 @@
+/** Service implementation for AdminService */
+module.exports = cds.service.impl(function() {
+  this.before ('CREATE', 'Orders', _checkOrderCreateAuth)
+})
+
+
+/** Check authorization  */
+function _checkOrderCreateAuth (req) {
+  req.user.currency[0] === req.data.currency_code || req.reject(403)
+}
