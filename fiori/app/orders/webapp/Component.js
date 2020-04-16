0ed239b28bbb318547da10df45a08cb47db4c257
Christian Georgi <christian.georgi@sap.com>
2020-04-16 15:21:05 +0200

    Fix usage of deprecated UI component

--- fiori/app/orders/webapp/Component.js
+++ fiori/app/orders/webapp/Component.js
@@ -1,22 +1,5 @@
-sap.ui.define(["sap/fe/AppComponent"], ac => ac.extend("orders.Component", {
+sap.ui.define(["sap/fe/core/AppComponent"], ac => ac.extend("orders.Component", {
     metadata:{ manifest:'json' }
 }))
-
-// sap.ui.define (["sap/ui/core/UIComponent"], ui5 => ui5.extend("bookshop.Component", {
-//     metadata: { manifest: "json" }
-// }))
-// sap.ui.define (["sap/ui/generic/app/AppComponent"], ui5 => ui5.extend("bookshop.Component", {
-//     metadata: { manifest: "json" }
-// }))
-
-// jQuery.sap.declare("bookshop.Component");
-// sap.ui.getCore().loadLibrary("sap.ui.generic.app");
-// jQuery.sap.require("sap.ui.generic.app.AppComponent");
-
-// sap.ui.generic.app.AppComponent.extend("bookshop.Component", {
-// 	metadata: {
-// 		manifest: "json"
-// 	}
-// });
 
 /* eslint no-undef:0 */
