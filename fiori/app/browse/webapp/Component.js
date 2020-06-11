a498ea954fca2bc0b879aea8aa0d8b7aba7e7d39
I051442 <gwendal.moreau@sap.com>
2020-06-11 15:35:06 +0200

    Update for sap.fe usage starting with UI5 1.78

--- fiori/app/browse/webapp/Component.js
+++ fiori/app/browse/webapp/Component.js
@@ -1,5 +1,7 @@
-sap.ui.define(["sap/fe/core/AppComponent"], ac => ac.extend("bookshop.Component", {
-    metadata:{ manifest:'json' }
-}))
-
+sap.ui.define(["sap/fe/core/AppComponent"], function(AppComponent) {
+	"use strict";
+	return AppComponent.extend("bookshop.Component", {
+		metadata: { manifest: "json" }
+	});
+});
 /* eslint no-undef:0 */
