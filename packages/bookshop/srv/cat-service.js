fca78bdb845761ae36bed1c3600cdabfd03879c7
Laszlo Kajan <laszlo.kajan@roche.com>
2020-09-17 22:18:17 +0200

    Update cat-service.js
    Update for "@sap/cds": "^4"

--- packages/bookshop/srv/cat-service.js
+++ packages/bookshop/srv/cat-service.js
@@ -15,6 +15,22 @@ function _addDiscount2 (each,discount) {
 /** Reduce stock of ordered books if available stock suffices */
 async function _reduceStock (req) {
   const { Items: OrderItems } = req.data
+  if (!Array.isArray(OrderItems)) { return; }
+
+  let updatePromises = OrderItems.map (order => {
+      return cds.transaction(req) .run (
+          UPDATE (Books) .set ('stock -=', order.amount)
+            .where ('ID =', order.book_ID) .and ('stock >=', order.amount)
+      );
+  });
+
+  return Promise.all(updatePromises).then(all => all.forEach ((affectedRows,i) => {
+    if (affectedRows === 0)  req.error (409,
+      `${OrderItems[i].amount} exceeds stock for book #${OrderItems[i].book_ID}`
+    )
+  }));
+
+  /*orig:
   return cds.transaction(req) .run (()=> OrderItems.map (order =>
     UPDATE (Books) .set ('stock -=', order.amount)
     .where ('ID =', order.book_ID) .and ('stock >=', order.amount)
@@ -22,7 +38,7 @@ async function _reduceStock (req) {
     if (affectedRows === 0)  req.error (409,
       `${OrderItems[i].amount} exceeds stock for book #${OrderItems[i].book_ID}`
     )
-  }))
+  }))*/
 }
 
 
