99861ca5886034c7f04cda3dc50c165a62ebe7e7
Laszlo Kajan <laszlo.kajan@roche.com>
2020-09-18 10:49:46 +0200

    Patch of _reduceStock() under @sap/cds 4 (#136)
    * Bump to cds 4
    * Cosmetics in handler code
      * await and cds.run([...]) instead of Promise.all
    
    Co-authored-by: Christian Georgi <christian.georgi@sap.com>

--- packages/bookshop/srv/cat-service.js
+++ packages/bookshop/srv/cat-service.js
@@ -14,16 +14,20 @@ function _addDiscount2 (each,discount) {
 
 /** Reduce stock of ordered books if available stock suffices */
 async function _reduceStock (req) {
-  const { Items: OrderItems } = req.data
-  return cds.transaction(req) .run (()=> OrderItems.map (order =>
-    UPDATE (Books) .set ('stock -=', order.amount)
-    .where ('ID =', order.book_ID) .and ('stock >=', order.amount)
-  )) .then (all => all.forEach ((affectedRows,i) => {
-    if (affectedRows === 0)  req.error (409,
-      `${OrderItems[i].amount} exceeds stock for book #${OrderItems[i].book_ID}`
-    )
-  }))
+  const { Items: orderItems } = req.data
+  if (!Array.isArray(orderItems))  return
+
+  const all = await cds.transaction(req).run(orderItems.map(item =>
+    UPDATE(Books)
+      .set('stock -=', item.amount)
+      .where('ID =', item.book_ID).and('stock >=', item.amount)
+  ))
+  all.forEach((affectedRows, i) => {
+    if (affectedRows === 0) {
+      req.error(409, `${orderItems[i].amount} exceeds stock for book #${orderItems[i].book_ID}`)
+    }
+  })
+
+ return all
+
 }
-
-
-
