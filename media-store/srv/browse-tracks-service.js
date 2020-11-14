7045914e57f38713f8a8dcbc9e51d1df040768e1
Dzmitry_Tamashevich@epam.com <diaamnj@gmail.com>
2020-11-14 23:22:11 +0300

    add env vars. change db kind

--- media-store/srv/browse-tracks-service.js
+++ media-store/srv/browse-tracks-service.js
@@ -1,30 +1,28 @@
 const cds = require("@sap/cds");
 
-const selectTracksByEmail = (email) => `
-    select tracks.ID
-    from sap_capire_media_store_Tracks tracks
-      join sap_capire_media_store_Invoices invoices 
-        on tracks.ID = invoiceItems.track_ID 
-      join sap_capire_media_store_InvoiceItems invoiceItems 
-        on invoices.ID = invoiceItems.invoice_ID  
-      join sap_capire_media_store_Customers customers 
-        on customers.ID = invoices.customer_ID
-      where (customers.email='${email}' and invoices.status='2') 
-        or (customers.email='${email}' and invoices.status='1')
-`;
-
 module.exports = async function () {
   const db = await cds.connect.to("db"); // connect to database service
 
+  const { Invoices, InvoiceItems } = db.entities;
+
   this.on("READ", "MarkedTracks", async (req) => {
-    const myTrackIds = (await db.run(selectTracksByEmail(req.user.id))).map(
-      ({ ID }) => ID
+    const invoiceItemEntries = await db.run(
+      SELECT.from(InvoiceItems)
+        .columns("track_ID")
+        .where(
+          "invoice_ID in",
+          SELECT("ID").from(Invoices).where({
+            customer_ID: req.user.attr.ID,
+          })
+        )
     );
+    const trackIds = invoiceItemEntries.map(({ track_ID }) => track_ID);
+
     const result = [];
     await db.foreach(req.query, (track) => {
       result.push({
         ...track,
-        alreadyOrdered: myTrackIds.includes(track.ID),
+        alreadyOrdered: trackIds.includes(track.ID),
       });
     });
     return result;
