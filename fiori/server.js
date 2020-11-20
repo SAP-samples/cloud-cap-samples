94092794f2107e32443e30a965683abb72704b1e
Daniel <daniel.hutzel@sap.com>
2020-11-20 16:08:49 +0100

    Revised layout of .html pages

--- fiori/server.js
+++ fiori/server.js
@@ -7,8 +7,11 @@ cds.once('bootstrap',(app)=>{
   const orders_app = dirname (require.resolve('@capire/orders/app/orders/webapp/manifest.json'))
   app.use ('/orders/webapp', express.static(orders_app))
   // serving the vue.js app imported from @capire/bookshop
-  const vue_app = dirname (require.resolve('@capire/bookshop/app/vue/index.html'))
-  app.use ('/vue', express.static(vue_app))
+  const bookshop_app = dirname (require.resolve('@capire/bookshop/app/vue/index.html'))
+  app.use ('/vue/bookshop', express.static(bookshop_app))
+  // serving the vue.js app imported from @capire/reviews
+  const reviews_app = dirname (require.resolve('@capire/reviews/app/vue/index.html'))
+  app.use ('/vue/reviews', express.static(reviews_app))
 })
 
 cds.once('served', require('./srv/mashup'))
