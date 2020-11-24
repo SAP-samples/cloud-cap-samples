85320787fbcf01a161561845eca98bdc64ca4e1e
Dzmitry_Tamashevich@epam.com <diaamnj@gmail.com>
2020-11-24 15:40:11 +0300

    now create tracks method works properly

--- media-store/srv/manage-store-service.js
+++ media-store/srv/manage-store-service.js
@@ -4,14 +4,22 @@ module.exports = async function () {
   const db = await cds.connect.to("db"); // connect to database service
 
   this.on("CREATE", "*", async (req) => {
+    const selectLastQuery = SELECT.one(req.entity)
+    .orderBy({ ID: "desc" });
+
     const transaction = await db.tx(req);
-    let { ID: lastEntityID } = await transaction.run(
-      SELECT.one(req.entity).columns("ID").orderBy({ ID: "desc" })
-    );
+
+    let { ID: lastEntityID } = await transaction.run(selectLastQuery);
+
     const columns = ["ID", ...Object.keys(req.data)];
     const values = [++lastEntityID, ...Object.values(req.data)];
+    const insertQuery = INSERT.into(req.entity).columns(columns).values(values);
 
-    await transaction.run(req.query.columns(columns).values(values));
+    await transaction.run(insertQuery);
+    const result = await transaction.run(selectLastQuery);
+
     await transaction.commit();
+
+    return result;
   });
 };
