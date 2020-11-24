38d6ed7ea4c1942b0e0ef4dcc9bea3540314a11c
Dmitriynj <diaamnj@gmail.com>
2020-11-24 12:49:26 +0300

    refactoring error page

--- media-store/srv/manage-store-service.js
+++ media-store/srv/manage-store-service.js
@@ -3,10 +3,15 @@ const cds = require("@sap/cds");
 module.exports = async function () {
   const db = await cds.connect.to("db"); // connect to database service
 
-  this.before("CREATE", "*", async (req) => {
-    let { ID: lastEntityID } = await db.run(
+  this.on("CREATE", "*", async (req) => {
+    const transaction = await db.tx(req);
+    let { ID: lastEntityID } = await transaction.run(
       SELECT.one(req.entity).columns("ID").orderBy({ ID: "desc" })
     );
-    req.data = { ...req.data, ID: ++lastEntityID };
+    const columns = ["ID", ...Object.keys(req.data)];
+    const values = [++lastEntityID, ...Object.values(req.data)];
+
+    await transaction.run(req.query.columns(columns).values(values));
+    await transaction.commit();
   });
 };
