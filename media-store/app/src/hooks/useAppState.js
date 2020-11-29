30adfeed5438bd3117a87a057456b469d6cf83d5
Dmitriynj <diaamnj@gmail.com>
2020-11-29 21:13:18 +0300

    add app to gitignore

--- media-store/app/src/hooks/useAppState.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { AppStateContext } from "../contexts/AppStateContext";
-
-const useAppState = () => useContext(AppStateContext);
-
-export { useAppState };
