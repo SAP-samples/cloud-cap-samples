62e18e24f5c02bf12ebfa2d7151d5dbe49c1d87e
Dmitriynj <diaamnj@gmail.com>
2020-11-29 21:32:14 +0300

    move xs-securiry.json

--- media-store/server.js
+++ media-store/server.js
@@ -2,31 +2,13 @@ const cds = require("@sap/cds");
 const {
   getDurationInMilliseconds,
   getFormattedDateTime,
+  corsMiddleware,
 } = require("./util/helpers");
 
 // handle bootstrapping events...
 cds.on("bootstrap", (app) => {
   if (cds.env.env === "development") {
-    app.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header(
-        "Access-Control-Allow-Methods",
-        "GET, PUT, PATCH, POST, DELETE, OPTIONS"
-      );
-      res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization, Accept-Language"
-      );
-
-      //intercepts OPTIONS method
-      if ("OPTIONS" === req.method) {
-        //respond with 200
-        res.sendStatus(200);
-      } else {
-        //move on
-        next();
-      }
-    });
+    app.use(corsMiddleware);
   }
 });
 cds.on("served", async ({ db, messaging, ...servedServices }) => {
