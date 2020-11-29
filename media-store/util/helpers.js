9e8ee9868c4507cfbd8d8ea09bd3ba283eb539b6
Dzmitry_Tamashevich@epam.com <diaamnj@gmail.com>
2020-11-29 21:32:14 +0300

    move xs-securiry.json

--- media-store/util/helpers.js
+++ media-store/util/helpers.js
@@ -22,7 +22,29 @@ const getFormattedDateTime = () => {
   return formattedDateTime;
 };
 
+const corsMiddleware = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, PATCH, POST, DELETE, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Accept-Language"
+  );
+
+  //intercepts OPTIONS method
+  if ("OPTIONS" === req.method) {
+    //respond with 200
+    res.sendStatus(200);
+  } else {
+    //move on
+    next();
+  }
+};
+
 module.exports = {
   getFormattedDateTime,
   getDurationInMilliseconds,
+  corsMiddleware,
 };
