d90c4e3a5b2993d6c4a5bda3d3ea237e24c0ed72
Dmitriynj <diaamnj@gmail.com>
2020-11-29 21:26:21 +0300

    change gitignore. change server.js

--- media-store/server.js
+++ media-store/server.js
@@ -1,4 +1,8 @@
 const cds = require("@sap/cds");
+const {
+  getDurationInMilliseconds,
+  getFormattedDateTime,
+} = require("./util/helpers");
 
 // handle bootstrapping events...
 cds.on("bootstrap", (app) => {
@@ -28,13 +32,26 @@ cds.on("bootstrap", (app) => {
 cds.on("served", async ({ db, messaging, ...servedServices }) => {
   // add logging current user before any request
   for (let i in servedServices) {
-    servedServices[i].prepend((srv) =>
-      srv.before("*", (req) => {
+    servedServices[i].prepend((srv) => {
+      srv.on("*", async (req, next) => {
+        const method = req._.req.method;
+        const url = req._.req.url;
+        const start = process.hrtime();
+
         if (req.user) {
           console.log("[USER]:", req.user.id, req.user.attr, req.user._roles);
         }
-      })
-    );
+        const result = await next();
+
+        const status = req._.res.statusCode;
+        const currentDateTime = getFormattedDateTime();
+        const durationInMilliseconds = getDurationInMilliseconds(start);
+        const log = `[${currentDateTime}] ${durationInMilliseconds.toLocaleString()} ms ${method}:${url} status: ${status} `;
+        console.log(log);
+
+        return result;
+      });
+    });
   }
 });
 
