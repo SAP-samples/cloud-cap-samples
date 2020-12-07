f32398ba8dd9b10c7f236282c3e1f89067e64c6f
Daniel <daniel.hutzel@sap.com>
2020-12-07 12:39:33 +0100

    Using cds.User.default in 4.4.4

--- test/odata.test.js
+++ test/odata.test.js
@@ -1,5 +1,7 @@
 const { GET, expect } = require('../test') .run ('bookshop')
-const cds = require('@sap/cds/lib'); cds.User = cds.User.Privileged // skip auth
+const cds = require('@sap/cds/lib')
+if (cds.User.default) cds.User.default = cds.User.Privileged // hard core monkey patch
+else cds.User = cds.User.Privileged // hard core monkey patch for older cds releases
 
 describe('OData Protocol', () => {
 
