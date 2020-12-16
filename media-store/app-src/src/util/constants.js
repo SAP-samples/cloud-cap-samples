343eb05ea6e0f40f7b8af8df411edaf3847777a8
Daniel <daniel.hutzel@sap.com>
2020-12-16 10:11:47 +0100

    Cleanup folder layout

--- media-store/app-src/src/util/constants.js
+++
@@ -1,7 +0,0 @@
-export const AVAILABLE_LOCALES = ['en', 'fr', 'de'];
-
-export const MESSAGE_TIMEOUT = 2;
-
-export const requireEmployee = (user) => !!user && user.roles.includes('employee');
-
-export const requireCustomer = (user) => !!user && user.roles.includes('customer');
