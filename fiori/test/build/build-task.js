d0734030108b1c3165d51d887c0f45c79f172888
Daniel <daniel.hutzel@sap.com>
2021-02-26 09:30:56 +0100

    cosmetics

--- fiori/test/build/build-task.js
+++ fiori/test/build/build-task.js
@@ -5,9 +5,9 @@ module.exports = class extends cds.build.Task {
   async build ({src='*'}) {
     this.log (`Generating edmx output for '${src}'...`)
     const csn = await this.model(src)
-    return Promise.all (csn.services.map (srv => {
-      const edmx = cds.compile(csn).to.edmx({service:srv.name})
-      return this.write(edmx).to(`{srv}/src/main/resources/${srv.name}.edmx`)
+    return Promise.all (csn.services.map (({name:service}) => {
+      const edmx = cds.compile(csn).to.edmx({service})
+      return this.write(edmx).to(`{srv}/src/main/resources/${service}.edmx`)
     }))
   }
 
