d670e57b0b17db902f444f36c4ce8f5550e55285
Christian Georgi <christian.georgi@sap.com>
2021-04-06 16:23:25 +0200

    Use cds.test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,2 @@
-
-const test = require('@sap/cds/lib/utils/tests').in(__dirname,'..')
-module.exports = Object.assign(test,{run:test})
-
-// REVISIT: With upcoming release of @sap/cds this should become:
-// module.exports = require('@sap/cds/tests').in(__dirname,'..')
+const cds = require('@sap/cds')
+module.exports = cds.test.in(__dirname,'..')
