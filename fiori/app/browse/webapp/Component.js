309c76caae520258282de8cc24640f62bb7168da
Wolfgang Koch <wolfgang.koch@sap.com>
2021-05-04 10:33:38 +0200

    adding fiori

---
+++ fiori/app/browse/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/fe/core/AppComponent"], function(AppComponent) {
+	"use strict";
+	return AppComponent.extend("bookshop.Component", {
+		metadata: { manifest: "json" }
+	});
+});
+/* eslint no-undef:0 */
