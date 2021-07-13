c04b972d27be2f67d753040740178f8700e62c7f
Christian Georgi <christian.georgi@sap.com>
2021-07-13 08:36:18 +0200

    Cosmetics

--- test/hello-world-ts.test.ts
+++ test/hello-world-ts.test.ts
@@ -2,14 +2,14 @@ process.env.CDS_TYPESCRIPT = 'true';
 import * as cds from '@sap/cds';
 
 //@ts-ignore
-const {GET} =  cds.test.in(__dirname,'../hello').run('serve', 'world.cds')
+const {GET} = cds.test.in(__dirname,'../hello').run('serve', 'world.cds');
 
 describe('Hello world!', () => {
     afterAll(() => { delete process.env.CDS_TYPESCRIPT; });
 
     it('should say hello with class impl from a typescript file', async () => {
         const {data} = await GET`/say/hello(to='world')`
-        expect(data.value).toEqual('Hello world from a typescript file!')
+        expect(data.value).toMatch(/Hello world.*typescript.*/i)
     })
 
 })
