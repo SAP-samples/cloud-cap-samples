6b6bcf54a7762021db834267717a5b8fc38104de
Christian Georgi <christian.georgi@sap.com>
2021-07-13 08:36:05 +0200

    Clear process variable after test

--- test/hello-world-ts.test.ts
+++ test/hello-world-ts.test.ts
@@ -5,6 +5,7 @@ import * as cds from '@sap/cds';
 const {GET} =  cds.test.in(__dirname,'../hello').run('serve', 'world.cds')
 
 describe('Hello world!', () => {
+    afterAll(() => { delete process.env.CDS_TYPESCRIPT; });
 
     it('should say hello with class impl from a typescript file', async () => {
         const {data} = await GET`/say/hello(to='world')`
