e19cb61d50586c35583ce2c3de85d4959c78e087
Pierre Fritsch <PierreFritsch@users.noreply.github.com>
2021-08-04 13:58:21 +0200

    hello TypeScript: Define type of `req` object
    Make it clearer that `req` is a CDS `Request` object.

--- hello/srv/world.ts
+++ hello/srv/world.ts
@@ -1,5 +1,7 @@
+import { Request } from "@sap/cds/apis/services"
+
 module.exports = class say {
-    hello(req: any) {
+    hello(req: Request) {
         return `Hello ${req.data.to} from a TypeScript file!`
     }
 }
