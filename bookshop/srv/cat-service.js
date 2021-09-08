2abf08c54ace4b1ecf9c9730fe9b42683570893a
Daniel <daniel.hutzel@sap.com>
2021-09-08 15:27:07 +0200

    Rename amount -> quantity

--- bookshop/srv/cat-service.js
+++ bookshop/srv/cat-service.js
@@ -5,14 +5,14 @@ class CatalogService extends cds.ApplicationService { init(){
 
   // Reduce stock of ordered books if available stock suffices
   this.on ('submitOrder', async req => {
-    const {book,amount} = req.data
+    const {book,quantity} = req.data
     let {stock} = await SELECT `stock` .from (Books,book)
-    if (stock >= amount) {
-      await UPDATE (Books,book) .with (`stock -=`, amount)
-      await this.emit ('OrderedBook', { book, amount, buyer:req.user.id })
+    if (stock >= quantity) {
+      await UPDATE (Books,book) .with (`stock -=`, quantity)
+      await this.emit ('OrderedBook', { book, quantity, buyer:req.user.id })
       return { stock }
     }
-    else return req.error (409,`${amount} exceeds stock for book #${book}`)
+    else return req.error (409,`${quantity} exceeds stock for book #${book}`)
   })
 
   // Add some discount for overstocked books
