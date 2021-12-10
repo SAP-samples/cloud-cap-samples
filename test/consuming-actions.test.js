02e1979c9fb6934e31f2999e8f8c8ba3c141c3dc
Pierre Fritsch <PierreFritsch@users.noreply.github.com>
2021-12-10 14:06:06 +0100

    Add assertions
    Signed-off-by: Pierre Fritsch <PierreFritsch@users.noreply.github.com>

--- test/consuming-actions.test.js
+++ test/consuming-actions.test.js
@@ -8,36 +8,47 @@ const { expect } = cds.test(
 );
 
 describe("Consuming actions locally", () => {
-  let cats, tx;
+  let cats, CatalogService, Books, tx, stockBefore;
+  const BOOK_ID = 251;
+  const QUANTITY = 1;
 
   before("bootstrap the database", async () => {
-    const { CatalogService } = cds.services;
+    CatalogService = cds.services.CatalogService;
     expect(CatalogService).not.to.be.undefined;
 
-    const { Books } = CatalogService.entities;
+    Books = CatalogService.entities.Books;
     expect(Books).not.to.be.undefined;
 
     cats = await cds.connect.to("CatalogService");
   });
 
-
   beforeEach(async () => {
+    // Read the stock before the action is called
+    stockBefore = (await cats.get(Books, BOOK_ID)).stock;
+
     // Use a manual transaction to create a continuation with an authenticated user
     tx = await cats.tx({user: "alice"});
   });
 
+  afterEach(async() => {
+    await tx.rollback();
+  })
+
   it("calls unbound actions - basic variant using srv.send", async () => {
     const res1 = await tx.send("submitOrder", {
-      book: 251,
-      quantity: 1,
+      book: BOOK_ID,
+      quantity: QUANTITY,
     });
+    expect(res1.stock).to.eql(stockBefore - QUANTITY);
   });
 
   it("calls unbound actions - named args variant", async () => {
-    const res2 = await tx.submitOrder({ book: 251, quantity: 1 });
+    const res2 = await tx.submitOrder({ book: BOOK_ID, quantity: QUANTITY });
+    expect(res2.stock).to.eql(stockBefore - QUANTITY);
   });
 
   it("calls unbound actions - positional args variant", async () => {
-    const res3 = await tx.submitOrder(251, 1);
+    const res3 = await tx.submitOrder(BOOK_ID, QUANTITY);
+    expect(res3.stock).to.eql(stockBefore - QUANTITY);
   });
 });
