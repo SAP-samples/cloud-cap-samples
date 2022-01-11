1a71a6d28a14e0d50f9ae5638d27bb3570d77de9
Christian Georgi <christian.georgi@sap.com>
2022-01-11 21:16:41 +0100

    Simple Data Viewer
    - Generic CDS service to fetch data
    - Simple Vue.js UI

--- bookstore/server.js
+++ bookstore/server.js
@@ -8,6 +8,7 @@ cds.once('bootstrap',(app)=>{
   app.serve ('/bookshop') .from ('@capire/bookshop','app/vue')
   app.serve ('/reviews') .from ('@capire/reviews','app/vue')
   app.serve ('/orders') .from('@capire/orders','app/orders')
+  app.serve ('/data') .from('@capire/data-viewer','app/viewer')
 })
 
 // Add Swagger UI
