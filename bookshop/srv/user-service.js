9370d0544ee013ca72ecf09e18ac1243493c304b
Daniel <daniel.hutzel@sap.com>
2022-05-12 01:44:09 +0200

    trying esm modules

--- bookshop/srv/user-service.js
+++ bookshop/srv/user-service.js
@@ -1,4 +1,4 @@
-const cds = require('@sap/cds')
-module.exports = cds.service.impl((srv) => {
+import cds from '@sap/cds'
+export default cds.service.impl((srv) => {
   srv.on('READ', 'me', ({ tenant, user, locale }) => ({ id: user.id, locale, tenant }))
 })
