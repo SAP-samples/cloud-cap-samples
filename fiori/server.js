5bd631d8a90799b19867b359300ef5b95d9162ea
Daniel <daniel.hutzel@sap.com>
2022-05-16 09:06:34 +0200

    Streamlined MTX

--- fiori/server.js
+++ fiori/server.js
@@ -1,1 +1,6 @@
 module.exports = require('@capire/bookstore/server.js')
+const cds = require ('@sap/cds')
+cds.on('bootstrap', (app) => app.use ((req,res,next) => {
+  req.features = req.headers.features?.split(',') || ['isbn']
+  next()
+}))
