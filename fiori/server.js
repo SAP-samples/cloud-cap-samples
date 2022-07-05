ac5e631a925a34ebb8c8e8a069a523606cf2098b
Christian Georgi <chgeo@users.noreply.github.com>
2022-07-05 17:46:15 +0200

    Tests for hierarchies (#375)

--- fiori/server.js
+++ fiori/server.js
@@ -1,5 +1,8 @@
-const cds = require("@sap/cds");
-const proxy = require("@sap/cds-odata-v2-adapter-proxy");
-cds.on("bootstrap", app => app.use(proxy()));
+const cds = require("@sap/cds")
+
+// install OData v2 adapter
+const proxy = require('@sap/cds-odata-v2-adapter-proxy')
+const proxyOpts = global.it ? { target:'auto' } : {} // for tests, set 'auto' to detect port dynamically
+cds.on('bootstrap', app => app.use(proxy(proxyOpts)))
 
 module.exports = require('@capire/bookstore/server.js')
