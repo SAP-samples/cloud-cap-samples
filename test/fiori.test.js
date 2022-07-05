226701fd779fbc6942ee62f73f7107097788eea9
Christian Georgi <christian.georgi@sap.com>
2022-07-05 17:23:28 +0200

    Tests for hierarchies

--- test/fiori.test.js
+++ test/fiori.test.js
@@ -2,8 +2,7 @@ const cds = require('@sap/cds/lib')
 const { GET, expect, axios } = cds.test ('@capire/fiori', '--with-mocks')
 axios.defaults.auth = { username: 'alice', password: 'admin' }
 
-// TODO enable if proxy-adapter 1.9 is out w/ support for ephemeral ports for testing
-describe.skip('cap/samples - Fiori APIs', () => {
+describe('cap/samples - Fiori APIs - v2', () => {
 
   it('serves $metadata documents in v2', async () => {
     const { headers, data } = await GET `/v2/browse/$metadata`
@@ -14,5 +13,10 @@ describe.skip('cap/samples - Fiori APIs', () => {
     expect(data).to.contain('<EntitySet Name="GenreHierarchy" EntityType="CatalogService.GenreHierarchy"/>')
   })
 
+  it('serves Books in v2', async () => {
+    const { data } = await GET `/v2/browse/Books`
+    expect(data).to.containSubset({d:{results:[]}})
+    expect(data.d.results.length).to.be.greaterThanOrEqual(5)
+  })
 
 })
