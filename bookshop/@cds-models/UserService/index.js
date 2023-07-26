b0baf3b5aa039ddbb86a4c611788eca2eba52e4d
Daniel Hutzel <daniel.hutzel@sap.com>
2023-07-26 17:08:47 +0200

    Delete bookshop/@cds-models directory

--- bookshop/@cds-models/UserService/index.js
+++
@@ -1,8 +0,0 @@
-// This is an automatically generated file. Please do not change its contents manually!
-const cds = require('@sap/cds')
-const csn = cds.entities('UserService')
-module.exports.me = csn.me
-module.exports.me_ = csn.me
-// actions
-module.exports.login = 'login'
-// enums
