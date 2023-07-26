86fb1d20d37d9db5340a8be9f9a9247824df0b17
Daniel Hutzel <daniel.hutzel@sap.com>
2023-07-26 17:06:08 +0200

    my-project -> compose

--- bookshop/@cds-models/AdminService/index.js
+++
@@ -1,9 +0,0 @@
-// This is an automatically generated file. Please do not change its contents manually!
-const cds = require('@sap/cds')
-const csn = cds.entities('AdminService')
-module.exports.Book = csn.Books
-module.exports.Books = csn.Books
-module.exports.Author = csn.Authors
-module.exports.Authors = csn.Authors
-// actions
-// enums
