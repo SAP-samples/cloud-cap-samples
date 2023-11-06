9b76e06dc13882cc49441f51ca7039fea3ee46c5
Daniel Hutzel <daniel.hutzel@sap.com>
2023-11-06 10:12:21 +0100

    Post TechEd cosmetics

--- bookshop/db/init.js
+++ bookshop/db/init.js
@@ -6,7 +6,7 @@
 
 // NOTE: We use cds.on('served') to delay the UPSERTs after the db init
 // to run after all INSERTs from .csv files happened.
-module.exports = cds.on('served', ()=> cds.run(
+module.exports = cds.on('served', ()=>
   UPSERT.into ('sap.common.Currencies') .columns (
     [ 'code', 'symbol', 'name' ]
   ) .rows (
@@ -16,4 +16,4 @@ module.exports = cds.on('served', ()=> cds.run(
     [ 'ILS', '₪', 'Shekel' ],
     [ 'JPY', '¥', 'Yen' ],
   )
-))
+)
--- bookshop/srv/cat-service.js
+++ bookshop/srv/cat-service.js
@@ -1,28 +1,37 @@
 const cds = require('@sap/cds')
 
-class CatalogService extends cds.ApplicationService { init(){
+class CatalogService extends cds.ApplicationService { init() {
 
-  const { Books } = cds.entities ('sap.capire.bookshop')
+  const { Books } = cds.entities('sap.capire.bookshop')
   const { ListOfBooks } = this.entities
 
-  // Reduce stock of ordered books if available stock suffices
-  this.on ('submitOrder', async req => {
-    const {book,quantity} = req.data
-    if (quantity < 1) return req.reject (400,`quantity has to be 1 or more`)
-    let b = await SELECT `stock` .from (Books,book)
-    if (!b) return req.error (404,`Book #${book} doesn't exist`)
-    let {stock} = b
-    if (quantity > stock) return req.reject (409,`${quantity} exceeds stock for book #${book}`)
-    await UPDATE (Books,book) .with ({ stock: stock -= quantity })
-    await this.emit ('OrderedBook', { book, quantity, buyer:req.user.id })
-    return { stock }
-  })
-
   // Add some discount for overstocked books
-  this.after ('READ', ListOfBooks, each => {
+  this.after('READ', ListOfBooks, each => {
     if (each.stock > 111) each.title += ` -- 11% discount!`
   })
 
+  // Reduce stock of ordered books if available stock suffices
+  this.on('submitOrder', async req => {
+    let { book:id, quantity } = req.data
+    let book = await SELECT.from (Books, id, b => b.stock)
+
+    // Validate input data
+    if (!book) return req.error (404, `Book #${id} doesn't exist`)
+    if (quantity < 1) return req.error (400, `quantity has to be 1 or more`)
+    if (quantity > book.stock) return req.error (409, `${quantity} exceeds stock for book #${id}`)
+
+    // Reduce stock in database and return updated stock value
+    await UPDATE (Books, id) .with ({ stock: book.stock -= quantity })
+    return book
+  })
+
+  // Emit event when an order has been submitted
+  this.after('submitOrder', async (_,req) => {
+    let { book, quantity } = req.data
+    await this.emit('OrderedBook', { book, quantity, buyer: req.user.id })
+  })
+
+  // Delegate requests to the underlying generic service
   return super.init()
 }}
 
