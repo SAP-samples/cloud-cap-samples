a30cfefd88675b5b313478ea0a49f6c25adfaf4d
Mara Kiefer <8320933+mnkiefer@users.noreply.github.com>
2024-04-12 08:45:51 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -3,17 +3,15 @@ const js = require('@eslint/js');
 const cds = require('@sap/eslint-plugin-cds');
 
 module.exports = {
-  {
-    "files": ["**/*.js"],
-    ...js.configs.recommended,
-    "languageOptions": {
-      "globals": {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.jest,
-        ...globals.mocha,
-        "es2022": true
-    }
+  "files": ["**/*.js"],
+  ...js.configs.recommended,
+  "languageOptions": {
+    "globals": {
+      ...globals.browser,
+      ...globals.node,
+      ...globals.jest,
+      ...globals.mocha,
+      "es2022": true
   },
   "rules": {
     ...js.configs.recommended.rules,
@@ -21,17 +19,17 @@ module.exports = {
     "require-atomic-updates": "off",
     "require-await": "warn",
     "no-unused-vars": ["warn", { "argsIgnorePattern": "_" }]
+  }
+},
+{ 
+  "files": ['**/*.cds', '**/*.csn', '**/*.csv'],
+  ...cds.configs.recommended,
+  "plugins": {
+    "@sap/cds": cds
   },
-  {
-    ...cds.configs.recommended,
-    "files": ['**/*.cds', '**/*.csn', '**/*.csv'],
-    "plugins": {
-      "@sap/cds": cds
-    },
-    "languageOptions": {
-      globals: {
-        ...cds.globals
-      }
+  "languageOptions": {
+    globals: {
+      ...cds.globals
     }
   }
 }
