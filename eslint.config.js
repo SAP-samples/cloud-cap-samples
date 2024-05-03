59867ef761ebb49785ae1cd8ea80f325b0dcc062
Jörg Mann <joerg.mann@sap.com>
2024-05-03 12:31:21 +0200

    simplify eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,31 +4,33 @@ const cds = require('@sap/eslint-plugin-cds')
 
 module.exports = [
   cds.configs.recommended,
+  js.configs.recommended,
   {
-    plugins: {
-      '@sap/cds': cds
-    },
-    rules: {
-      ...cds.configs.recommended.rules
-    }
-  },
-  {
-    files: ['**/*.js'],
-    ...js.configs.recommended,
     languageOptions: {
       globals: {
+        es2022: true,
         ...globals.browser,
         ...globals.node,
         ...globals.jest,
         ...globals.mocha,
-        es2022: true
+        cds: true,
+        sap: true,
+        CDL: true,
+        CQL: true,
+        CREATE: true,
+        DELETE: true,
+        DROP: true,
+        INSERT: true,
+        SELECT: true,
+        UPDATE: true,
+        UPSERT: true
       }
     },
     rules: {
       'no-console': 'off',
       'require-atomic-updates': 'off',
       'require-await': 'warn',
-      'no-unused-vars': ['warn', { 'argsIgnorePattern': '_' }]
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
     }
   }
 ]
