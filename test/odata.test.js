aaa94e2d5f92bed5ddeda568436327636862c649
Daniel Hutzel <daniel.hutzel@sap.com>
2024-05-14 14:37:37 +0200

    Improve tests to also work with node --test

--- test/odata.test.js
+++ test/odata.test.js
@@ -28,63 +28,66 @@ describe('cap/samples - Bookshop APIs', () => {
     ])
   })
 
-  it('supports $search in multiple fields', async () => {
-    const { data } = await GET `/browse/Books ${{
-      params: { $search: 'Po', $select: `title,author` },
-    }}`
-    expect(data.value).to.containSubset([
-      { ID: 201, title: 'Wuthering Heights', author: 'Emily Brontë' },
-      { ID: 207, title: 'Jane Eyre', author: 'Charlotte Brontë' },
-      { ID: 251, title: 'The Raven', author: 'Edgar Allen Poe' },
-      { ID: 252, title: 'Eleonora', author: 'Edgar Allen Poe' },
-    ])
-  })
+  describe('query options...', () => {
 
-  it('supports $select', async () => {
-    const { data } = await GET(`/browse/Books`, {
-      params: { $select: `ID,title` },
+    it('supports $search in multiple fields', async () => {
+      const { data } = await GET `/browse/Books ${{
+        params: { $search: 'Po', $select: `title,author` },
+      }}`
+      expect(data.value).to.containSubset([
+        { ID: 201, title: 'Wuthering Heights', author: 'Emily Brontë' },
+        { ID: 207, title: 'Jane Eyre', author: 'Charlotte Brontë' },
+        { ID: 251, title: 'The Raven', author: 'Edgar Allen Poe' },
+        { ID: 252, title: 'Eleonora', author: 'Edgar Allen Poe' },
+      ])
     })
-    expect(data.value).to.containSubset([
-      { ID: 201, title: 'Wuthering Heights' },
-      { ID: 207, title: 'Jane Eyre' },
-      { ID: 251, title: 'The Raven' },
-      { ID: 252, title: 'Eleonora' },
-      { ID: 271, title: 'Catweazle' },
-    ])
-  })
 
-  it('supports $expand', async () => {
-    const { data } = await GET(`/admin/Authors`, {
-      params: {
-        $select: `name`,
-        $expand: `books($select=title)`,
-      },
+    it('supports $select', async () => {
+      const { data } = await GET(`/browse/Books`, {
+        params: { $select: `ID,title` },
+      })
+      expect(data.value).to.containSubset([
+        { ID: 201, title: 'Wuthering Heights' },
+        { ID: 207, title: 'Jane Eyre' },
+        { ID: 251, title: 'The Raven' },
+        { ID: 252, title: 'Eleonora' },
+        { ID: 271, title: 'Catweazle' },
+      ])
     })
-    expect(data.value).to.containSubset([
-      { name: 'Emily Brontë', books: [{ title: 'Wuthering Heights' }] },
-      { name: 'Charlotte Brontë', books: [{ title: 'Jane Eyre' }] },
-      { name: 'Edgar Allen Poe', books: [{ title: 'The Raven' }, { title: 'Eleonora' }] },
-      { name: 'Richard Carpenter', books: [{ title: 'Catweazle' }] },
-    ])
-  })
 
-  it('supports $value requests', async () => {
-    const { data } = await GET`/admin/Books/201/stock/$value`
-    expect(data).to.equal(12)
-  })
+    it('supports $expand', async () => {
+      const { data } = await GET(`/admin/Authors`, {
+        params: {
+          $select: `name`,
+          $expand: `books($select=title)`,
+        },
+      })
+      expect(data.value).to.containSubset([
+        { name: 'Emily Brontë', books: [{ title: 'Wuthering Heights' }] },
+        { name: 'Charlotte Brontë', books: [{ title: 'Jane Eyre' }] },
+        { name: 'Edgar Allen Poe', books: [{ title: 'The Raven' }, { title: 'Eleonora' }] },
+        { name: 'Richard Carpenter', books: [{ title: 'Catweazle' }] },
+      ])
+    })
 
-  it('supports $top/$skip paging', async () => {
-    const { data: p1 } = await GET`/browse/Books?$select=title&$top=3`
-    expect(p1.value).to.containSubset([
-      { ID: 201, title: 'Wuthering Heights' },
-      { ID: 207, title: 'Jane Eyre' },
-      { ID: 251, title: 'The Raven' },
-    ])
-    const { data: p2 } = await GET`/browse/Books?$select=title&$skip=3`
-    expect(p2.value).to.containSubset([
-      { ID: 252, title: 'Eleonora' },
-      { ID: 271, title: 'Catweazle' },
-    ])
+    it('supports $value requests', async () => {
+      const { data } = await GET`/admin/Books/201/stock/$value`
+      expect(data).to.equal(12)
+    })
+
+    it('supports $top/$skip paging', async () => {
+      const { data: p1 } = await GET`/browse/Books?$select=title&$top=3`
+      expect(p1.value).to.containSubset([
+        { ID: 201, title: 'Wuthering Heights' },
+        { ID: 207, title: 'Jane Eyre' },
+        { ID: 251, title: 'The Raven' },
+      ])
+      const { data: p2 } = await GET`/browse/Books?$select=title&$skip=3`
+      expect(p2.value).to.containSubset([
+        { ID: 252, title: 'Eleonora' },
+        { ID: 271, title: 'Catweazle' },
+      ])
+    })
   })
 
   it('serves user info', async () => {
