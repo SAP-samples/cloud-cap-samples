01f724f6a882feca0ea9e46189f6ad010940ca12
Daniel Schlachter <schlachter.daniel@gmail.com>
2024-08-07 11:07:37 +0000

    concrete error message

--- bookstore/server.js
+++ bookstore/server.js
@@ -5,10 +5,14 @@ cds.once('served', require('./srv/mashup'))
 
 // Add routes to UIs from imported packages
 cds.once('bootstrap',(app)=>{
-  app.serve ('/bookshop') .from ('@capire/bookshop','app/vue')
-  app.serve ('/reviews') .from ('@capire/reviews','app/vue')
-  app.serve ('/orders') .from('@capire/orders','app/orders')
-  app.serve ('/data') .from('@capire/data-viewer','app/viewer')
+  try {
+    app.serve ('/bookshop') .from ('@capire/bookshop','app/vue')
+    app.serve ('/reviews') .from ('@capire/reviews','app/vue')
+    app.serve ('/orders') .from('@capire/orders','app/orders')
+    app.serve ('/data') .from('@capire/data-viewer','app/viewer')
+  } catch {
+      throw new Error('Run "npm ci" to install the required dependencies')
+  }
 })
 
 // Add Swagger UI
