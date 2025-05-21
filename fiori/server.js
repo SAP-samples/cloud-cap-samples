332e6d4a205be6786a2a3157e50f65862fd37a05
Daniel Hutzel <daniel.hutzel@sap.com>
2025-05-21 06:04:26 +0200

    Implementing @hierarchy shortcut

--- fiori/server.js
+++ fiori/server.js
@@ -1,16 +1,50 @@
-// PoC for simplified Fiori Tree Views
 const cds = require('@sap/cds/lib')
 
-cds.on('compile.to.edmx', csn => {
-  const { 'AdminService.Genres': Genres } = csn.definitions
-
-  Genres['@Hierarchy.RecursiveHierarchy#GenreHierarchy.$Type'] = 'Hierarchy.RecursiveHierarchyType'
-  Genres['@Hierarchy.RecursiveHierarchy#GenreHierarchy.LimitedDescendantCount'] = {'=':'LimitedDescendantCount'}
-  Genres['@Hierarchy.RecursiveHierarchy#GenreHierarchy.DistanceFromRoot'] = {'=':'DistanceFromRoot'}
-  Genres['@Hierarchy.RecursiveHierarchy#GenreHierarchy.DrillState'] = {'=':'DrillState'}
-  Genres['@Hierarchy.RecursiveHierarchy#GenreHierarchy.LimitedRank'] = {'=':'LimitedRank'}
-
-  Genres['@Aggregation.RecursiveHierarchy#GenreHierarchy.$Type'] = 'Aggregation.RecursiveHierarchyType'
-  Genres['@Aggregation.RecursiveHierarchy#GenreHierarchy.NodeProperty'] = {'=':'ID'}
-  Genres['@Aggregation.RecursiveHierarchy#GenreHierarchy.ParentNavigationProperty'] = {'=':'parent'}
+// PoC for simplified Fiori Tree Views
+cds.on('compile.for.runtime', csn => {
+  for (let each of cds.linked(csn).definitions) {
+    if (each.is_entity && each._service && each['@hierarchy']) _hierarchy (each)
+  }
 })
+
+
+const _hierarchy = entity => {
+
+  // Add annotations explaining the hierarchy structure to Fiori
+  const Qualifier = entity.name.slice (entity._service.name.length+1) + 'Hierarchy'
+  const parent = _parent4(entity)
+  entity[`@Aggregation.RecursiveHierarchy#${Qualifier}.ParentNavigationProperty`] ??= {'=': parent.name }
+  entity[`@Aggregation.RecursiveHierarchy#${Qualifier}.NodeProperty`] ??= {'=': parent.keys[0].ref[0] }
+
+  // Add expected hierarchy elements to the entity
+  const columns = entity.projection.columns ??= ['*']
+  const elements = entity.elements
+  for (let e of Hierarchy.elements) {
+    entity[`@Hierarchy.RecursiveHierarchy#${Qualifier}.${e.name}`] = {'=': e.name }
+    if (e.name in elements) continue
+    const { name, value, ...rest } = e
+    elements[e.name] = Object.defineProperty ({ __proto__:e, ...rest }, 'parent', { value: entity })
+    columns.push ({ ...value, as: name, cast: { type: e.type } })
+  }
+
+  // Disable filter and sort for hierarchy elements
+  entity['@Capabilities.FilterRestrictions.NonFilterableProperties'] =
+  entity['@Capabilities.SortRestrictions.NonSortableProperties'] =
+  Object.keys (Hierarchy.elements)
+}
+
+
+const _parent4 = entity => {
+  const parent = entity['@hierarchy.parent'] || entity['@hierarchy.via']
+  if (parent) return entity.elements [parent['=']||parent]
+  else for (let e of entity.elements) // use first recursive uplink association
+    if (e.is2one && e._target === entity) return e
+}
+
+
+const { Hierarchy } = cds.linked `aspect Hierarchy {
+  LimitedDescendantCount : Int16 = null;
+  DistanceFromRoot : Int16 = null;
+  DrillState : String = null;
+  LimitedRank : Int16 = null;
+}`.definitions
